2c59eefd95831f777210fb4a46447b828dcfc7fa
Andoni Alonso Tort <andoni.alonso-tort@etu.univ-lehavre.fr>
2022-01-13 13:46:05 +0100

    Methode pour Manchester Differentiel terminé et arrengement du code

--- src/ex/DessinNRZ.java
+++ src/ex/DessinNRZ.java
@@ -21,6 +21,7 @@ public class DessinNRZ extends Canvas implements Dess{
 		
 		public int haut = 100;
 		public int bas = 300;
+		public int izq = 100;
 		
 		public DessinNRZ() {
 		}
@@ -93,7 +94,10 @@ public class DessinNRZ extends Canvas implements Dess{
 	        }
 		}
 		
-		
+		/**
+		 * Methode NRZ
+		 * @param g
+		 */
 		public void nrz(Graphics g) {
 			creationBase(g);
 			x_av = 100;
@@ -126,32 +130,9 @@ public class DessinNRZ extends Canvas implements Dess{
 			 			g.drawLine(x_av, y_av, x_ap, y_ap);
 			 			y_av = y_ap;
 			 		}
-
-			 	}
-			 				 		
+			 	}		 		
 			 	x_ap = x_ap + 50;	
-			 	
 			 }
-
-		}
-		
-		private boolean change(int n, char c) {
-			boolean change;
-			if(str.charAt(n+1) != c) {
-				change = true;
-			} else {
-				change = false;
-			}
-			return change;
-		}
-		
-		
-		public void miller(Graphics g) {
-			creationBase(g);
-		}
-		
-		public void manchesterDiff(Graphics g) {
-			creationBase(g);
 		}
 		
 		public void manchester(Graphics g) {
@@ -177,7 +158,84 @@ public class DessinNRZ extends Canvas implements Dess{
 			}
 			
 		}
-
+		
+		/**
+		 * Methode Manchester Differentiel
+		 * @param g
+		 */
+		public void manchesterDiff(Graphics g) {
+			creationBase(g);
+			
+			x_av = izq;
+			x_ap = x_av;
+			y_av = haut;
+			y_ap = haut;
+			
+			for (int n=0; n < str.length(); n++) {
+			 	char c = str.charAt (n);
+			 	
+			 	if(c == '1') {
+			 		faireChemin(g);
+			 	} else {
+			 		faireLigneVerticale(g);
+			 		faireChemin(g);
+			 	}
+			}
+			
+			
+		}
+		
+		/**
+		 * Methode Miller
+		 * @param g
+		 */
+		public void miller(Graphics g) {
+			creationBase(g);
+		}
+		
+		/**
+		 * Dessiner les lignes pour Manchester
+		 * @param g
+		 * @param valeur
+		 */
+		public void condMan(Graphics g, int valeur) {
+			// Si la valeur est 1 cela vaut dire que nous allons partir en haut
+			if(valeur == 1) {
+				y_av = haut;
+				y_ap = haut;
+			} else {
+				// Sinon on part en bas
+				y_av = bas;
+				y_ap = bas;
+			}
+			
+			
+			g.drawLine(x_av, y_av, x_ap, y_ap);
+			
+			x_av = x_ap;
+			
+			if(valeur == 1) {
+				y_ap = bas;
+			} else {
+				y_ap = haut;
+			}
+			
+			g.drawLine(x_av, y_av, x_ap, y_ap);
+			
+			x_ap = x_ap + 25;
+			y_av = y_ap;
+			g.drawLine(x_av, y_av, x_ap, y_ap);
+			
+			x_av = x_ap;
+			x_ap = x_ap + 25;
+		}
+		
+		// ==================== D'autres méthodes =====================================
+		
+		/**
+		 * Methode pour creer le tableau vide
+		 * @param g
+		 */
 		public void creationBase(Graphics g) {
 			Graphics2D g2d = (Graphics2D)g;
 			// Obtenir le le maximum
@@ -223,36 +281,72 @@ public class DessinNRZ extends Canvas implements Dess{
 			
 		}
 		
-		// Dessiner les lignes pour Manchester
-		public void condMan(Graphics g, int valeur) {
-			// Si la valeur est 1 cela vaut dire que nous allons partir en haut
-			if(valeur == 1) {
-				y_av = haut;
-				y_ap = haut;
+		/**
+		 * Methode pour savoir s'il y aura un changement
+		 * @param n
+		 * @param c
+		 * @return
+		 */
+		private boolean change(int n, char c) {
+			boolean change;
+			if(str.charAt(n+1) != c) {
+				change = true;
 			} else {
-				// Sinon on part en bas
+				change = false;
+			}
+			return change;
+		}
+		
+		/**
+		 * Incrementation de la variable puor avancer
+		 */
+		public void xApresAvancer() {
+			x_ap = x_av + 25;
+		}
+		
+		/**
+		 * Incrementation de x_av
+		 */
+		public void xAvantAvancer() {
+			x_av = x_ap;
+		}
+		
+		/**
+		 * Methode pour dessiner le chemin du nombre correspondant
+		 * @param g
+		 */
+		public void faireChemin(Graphics g) {
+			xApresAvancer();
+			draw(g);
+			faireLigneVerticale(g);
+			xApresAvancer();
+			draw(g);
+			xAvantAvancer();
+			
+		}
+		
+		/**
+		 * Faire une ligne Verticale
+		 * @param g
+		 */
+		public void faireLigneVerticale(Graphics g) {
+			x_av = x_ap;
+			if(y_ap == haut ) {
+				y_ap = bas;
+				draw(g);
 				y_av = bas;
-				y_ap = bas;
-			}
-			
-			
-			g.drawLine(x_av, y_av, x_ap, y_ap);
-			
-			x_av = x_ap;
-			
-			if(valeur == 1) {
-				y_ap = bas;
 			} else {
 				y_ap = haut;
+				draw(g);
+				y_av = haut;
 			}
-			
+		}
+		
+		/**
+		 * Methode pour dessiner dans la position actuelle
+		 * @param g
+		 */
+		public void draw(Graphics g) {
 			g.drawLine(x_av, y_av, x_ap, y_ap);
-			
-			x_ap = x_ap + 25;
-			y_av = y_ap;
-			g.drawLine(x_av, y_av, x_ap, y_ap);
-			
-			x_av = x_ap;
-			x_ap = x_ap + 25;
 		}
 }
